87384aae9bc11a99093eaf5282107bb7d1ab9f0c
12ball <pub@janie.page>
2025-10-12 18:36:56 -0500

    1760312216

--- client.js
+++ client.js
@@ -1,38 +1,39 @@
 const [_t, _s, host, user] = Bun.argv;
 
 while (true) {
-    let res;
-    const res1 = await fetch(`http://${host}/tic/games`);
-    if (res1.ok) {
-        res = await res1.json()
-    } else {
-        continue;
-    }
-    console.log("loaded games");
-    for (const game of res) {
-        if (game.turn === user) {
-            console.log(game.id)
-            while (true) {
-                const i = Math.floor(Math.random() * 3);
-                const j = Math.floor(Math.random() * 3);
-                if (game.state[i][j] === "_") {
-                    const body = {
-                        id: game.id,
-                        user: user,
-                        row: i,
-                        col: j,
-                    };
-                    console.log(game);
-                    console.log(body);
-                    fetch(`http://${host}/tic/move`, {
-                        method: "POST",
-                        headers: { "Content-Type": "application/json" },
-                        body: JSON.stringify(body),
-                    });
-                    break;
-                }
-            }
+  let res;
+  const res1 = await fetch(`http://${host}/tic/games`);
+  if (res1.ok) {
+    res = await res1.json();
+  } else {
+    continue;
+  }
+  console.log("loaded games");
+  for (const game of res) {
+    if (game.turn === user) {
+      console.log(game.id);
+      while (true) {
+        const i = Math.floor(Math.random() * 3);
+        const j = Math.floor(Math.random() * 3);
+        if (game.state[i][j] === "_") {
+          const body = {
+            id: game.id,
+            user: user,
+            row: i,
+            col: j,
+          };
+          console.log(game);
+          console.log(body);
+          fetch(`http://${host}/tic/move`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(body),
+          });
+          break;
         }
+      }
     }
-    await new Promise(resolve => setTimeout(resolve, 1000));
+  }
+  console.log("sleep");
+  await new Promise((resolve) => setTimeout(resolve, 1000));
 }
