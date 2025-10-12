42f818d8a4b8ac70133d611a0648698b856ed3ca
12ball <pub@janie.page>
2025-10-12 13:35:54 -0500

    1760294154

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 	"time"
 
@@ -8,7 +10,21 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+type Game struct {
+	Id    string     `json:"id"`
+	Turn  string     `json:"turn"`
+	Piece string     `json:"piece"`
+	State [][]string `json:"state"`
+}
+
 func main() {
+	err := run()
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func run() error {
 	r := chi.NewRouter()
 
 	r.Use(middleware.RequestID)
@@ -17,9 +33,28 @@ func main() {
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Timeout(60 * time.Second))
 
+	games := make(map[string]Game)
+	games["2ca973fc-b37d-4f3b-9e25-81360f430a84"] = Game{
+		Id:    "2ca973fc-b37d-4f3b-9e25-81360f430a84",
+		Turn:  "gwen",
+		Piece: "X",
+		State: [][]string{[]string{"_", "_", "_"}, []string{"_", "_", "_"}, []string{"_", "_", "_"}},
+	}
+
 	r.Get("/tic", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("yes i am alive"))
 	})
+	r.Get("/tic/challenges", func(w http.ResponseWriter, r *http.Request) {
+		res, err := json.Marshal(games)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		} else {
+			w.Header().Set("Content-Type", "application/json")
+			if _, err := w.Write(res); err != nil {
+				log.Println("error /tic/challenges", err)
+			}
+		}
+	})
 
-	http.ListenAndServe(":3333", r)
+	return http.ListenAndServe(":3333", r)
 }
