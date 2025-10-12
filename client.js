cd2d24e62f8eebfcae52a57cb1771d4ca5db9481
12ball <pub@janie.page>
2025-10-12 17:08:33 -0500

    1760306913

--- client.js
+++ client.js
@@ -3,10 +3,11 @@ while (true) {
     r.json()
   );
 
-  for (const game in res) {
+  for (const game of res) {
+    console.log(game);
     if (game.turn === "jane") {
       for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; i++) {
+        for (let j = 0; j < 3; j++) {
           if (game.state[i][j] === "_") {
             fetch("http://192.168.1.82/tic/move", {
               method: "POST",
