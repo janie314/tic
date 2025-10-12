a99d1de2df8b5c26d4988a2cb0781ed2810846fb
12ball <pub@janie.page>
2025-10-12 12:59:35 -0500

    2

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module tic/m
 
 go 1.25.2
 
-require github.com/go-chi/chi/v5 v5.2.3 // indirect
+require github.com/go-chi/chi/v5 v5.2.3
--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"net/http"
 	"time"
 
@@ -19,76 +17,9 @@ func main() {
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Timeout(60 * time.Second))
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+	r.Get("/tic", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("yes i am alive"))
 	})
 
-	// RESTy routes for "articles" resource
-	r.Route("/articles", func(r chi.Router) {
-		r.With(paginate).Get("/", listArticles)                           // GET /articles
-		r.With(paginate).Get("/{month}-{day}-{year}", listArticlesByDate) // GET /articles/01-16-2017
-
-		r.Post("/", createArticle)       // POST /articles
-		r.Get("/search", searchArticles) // GET /articles/search
-
-		// Regexp url parameters:
-		r.Get("/{articleSlug:[a-z-]+}", getArticleBySlug) // GET /articles/home-is-toronto
-
-		// Subrouters:
-		r.Route("/{articleID}", func(r chi.Router) {
-			r.Use(ArticleCtx)
-			r.Get("/", getArticle)       // GET /articles/123
-			r.Put("/", updateArticle)    // PUT /articles/123
-			r.Delete("/", deleteArticle) // DELETE /articles/123
-		})
-	})
-
-	// Mount the admin sub-router
-	r.Mount("/admin", adminRouter())
-
 	http.ListenAndServe(":3333", r)
-}
-
-func ArticleCtx(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		articleID := chi.URLParam(r, "articleID")
-		article, err := dbGetArticle(articleID)
-		if err != nil {
-			http.Error(w, http.StatusText(404), 404)
-			return
-		}
-		ctx := context.WithValue(r.Context(), "article", article)
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
-}
-
-func getArticle(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-	article, ok := ctx.Value("article").(*Article)
-	if !ok {
-		http.Error(w, http.StatusText(422), 422)
-		return
-	}
-	w.Write([]byte(fmt.Sprintf("title:%s", article.Title)))
-}
-
-// A completely separate router for administrator routes
-func adminRouter() http.Handler {
-	r := chi.NewRouter()
-	r.Use(AdminOnly)
-	r.Get("/", adminIndex)
-	r.Get("/accounts", adminListAccounts)
-	return r
-}
-
-func AdminOnly(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		perm, ok := ctx.Value("acl.permission").(YourPermissionType)
-		if !ok || !perm.IsAdmin() {
-			http.Error(w, http.StatusText(403), 403)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
 }
